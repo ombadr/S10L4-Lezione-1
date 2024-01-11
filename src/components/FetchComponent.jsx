861f91cbe63730a982053f5e01b3c95dcc8d9562
Omar Badr <omarbadr938@gmail.com>
2024-01-11 11:38:29 +0100

    x

--- src/components/FetchComponent.jsx
+++ src/components/FetchComponent.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Form, ListGroup } from 'react-bootstrap';
 
 const FetchComponent = () => {
-  return <div>FetchComponent</div>;
+  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      if (response.ok) {
+        const users = await response.json();
+        setData(users);
+      } else {
+        throw new Error('Error in data retrieval');
+      }
+    } catch (err) {
+      console.log('Error: ', err);
+    }
+  };
+
+  return (
+    <ListGroup className='mt-5'>
+      <Form.Control
+        placeholder='Search for a user...'
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      {data
+        .filter((user) =>
+          user.name.toLowerCase().includes(searchValue.toLowerCase())
+        )
+        .map((user) => {
+          return (
+            <ListGroup.Item key={user.id} data-testid='listgroup-item'>
+              {user.name} - {user.email} - {user.website}
+            </ListGroup.Item>
+          );
+        })}
+    </ListGroup>
+  );
 };
 
 export default FetchComponent;
